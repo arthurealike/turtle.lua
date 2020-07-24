7f8e31e865bf7319575874981c5594b3833f4bd0
arthurah <thebestarthure@hotmail.com>
2020-07-24 19:50:38 +0300

    Migrate to oop

--- main.lua
+++ main.lua
@@ -1,37 +1,57 @@
 local turtle = require "turtle"
 
+local fps 
+
+local w, h = love.graphics.getWidth(), love.graphics.getHeight()
+
+local t = turtle()
+
 function snowflake(l, c)
+    if c > 7 then 
+        print("Do not exceed number 7")
+        return nil
+    end
     if c < 1 then
-        turtle:forward(l)
+        t:forward(l)
         return nil
     end
     l = l / 3
     snowflake(l, c-1)
-    turtle:left(60)
+    t:left(60)
     snowflake(l, c-1)
-    turtle:right(120)
+    t:right(120)
     snowflake(l, c-1)
-    turtle:left(60)
+    t:left(60)
     snowflake(l, c-1)
 end
 
+function love.wheelmoved(x, y)
+    iszoom = true
+
+    sx = sy + y / 120
+    sy = sy + y / 120
+end
+
 function love.load()
-    turtle:init()
+    --  turtle:init()
+    local cl = {1, 0, 1}
+    t:setcolor(cl):clear()
     local l = 300
-    turtle:penup()
-    turtle:backward(l / 2)
-    turtle:pendown()
 
     for i=1, 3 do
-        snowflake(300, 3)
-        turtle:right(120)
+        snowflake(300, 5)
+        t:right(120)
     end
 end
 
 function love.update(dt)
-
+    fps = love.timer.getFPS()
+    mx, my = love.mouse.getPosition() 
 end
 
 function love.draw()
-    turtle:draw()
+    love.graphics.print(fps)
+    --  love.graphics.translate(w -mx, h-my)
+    love.graphics.scale(sx, sy)
+    t:draw()
 end
--- turtle.lua
+++ turtle.lua
@@ -1,36 +1,50 @@
-local turtle = {
-    currentPosition = {x = 0, y = 0} ,
-    cursorPosition = {x = 0, y = 0} ,
-    lineSegments = {} ,
-    drawing = true ,
-    direction = 0 , 
-    color = {r=1, g=1, b=0} ,
-    turtleVisibility = true ,
-    size = 1 ,
-    speed = 0.1
-}
+local turtle = {}
 
 turtle.__index = turtle
 
-local newline = function(x1, y1, x2, y2) 
-    local line = {p1 = {x = x1, y = y1 }, p2 = {x = x2, y = y2}}
-    return line
+local newline = function(x1, y1, x2, y2, c) 
+    return {p1 = {x = x1, y = y1 }, p2 = {x = x2, y = y2} , c = c or {1, 0, 0}}
 end
 
-function turtle:init(x, y, color, speed)
-    self.currentPosition.x = x or love.graphics.getWidth() / 2
-    self.currentPosition.y = y or love.graphics.getHeight() / 2
-    self.color = color or {1, 0, 0}
-    self.speed = speed or 1
-    self.direction = 0 
+local getcolor = function(color, mode)  
+    local c = color
+    if mode ~= false then
+        c = {love.math.random(0, 1), love.math.random(0, 1),love.math.random(0, 1)}
+    end 
+    return c
+end 
+
+local function new(x, y, speed, color) 
+    linesegments = {}
+    currentposition = {x = x, y = y} or {x = love.graphics.getWidth(), y = love.graphics.getHeight()}
+    speed = speed or 1
+    color = color or {1, 1, 0}
+    direction = 0
+    size = 1
+    rainbowmode = false
+    turtleVisibility = true
+    return setmetatable({linesegments = linesegments, currentposition = currentposition, direction = direction, color = color, speed = speed}, turtle)
 end
 
-function turtle:getLines() 
+function turtle:setcolor(...)
+    local c = self.color
+    local size = select("#", ...)
+    if size == 3 then
+        c = {...}
+    elseif size == 1 then 
+        c = ... 
+    end
+    self.color = c
+    return self
+end
+
+function turtle:getlines() 
     return self.lineSegments
 end
 
 function turtle:draw()
-    for _,line in pairs(self.lineSegments) do
+    for _,line in pairs(self.linesegments) do
+        love.graphics.setColor(line.c)
         love.graphics.line(line.p1.x,line.p1.y, line.p2.x, line.p2.y)
     end
 end
@@ -39,22 +53,33 @@ function turtle:clear()
     love.graphics.setColor(1, 1, 1)
     self.lineSegments = {}
     love.graphics.setLineWidth(1)
+    self.rainbowmode = false
+    return self
 end
 
 function turtle:penup()
     self.drawing = false
+    return self
 end
 
 function turtle:pendown()
     self.drawing = true
+    return self
+end
+
+function turtle:rainbow()
+    self.rainbowmode = true
+    return self
 end
 
 function turtle:showturtle()
     self.turtleVisibility = true
+    return self
 end
 
 function turtle:hideturtle()
     self.turtleVisibility = false
+    return self
 end
 
 function turtle:isvisible()
@@ -71,30 +96,40 @@ function turtle:setspeed(speed)
 end
 
 function turtle:forward(distance)
-    local x, y = self.currentPosition.x + distance * math.cos(self.direction) , self.currentPosition.y + distance * math.sin(self.direction)
-    local line = newline(self.currentPosition.x, self.currentPosition.y, x, y) 
+    local x, y = self.currentposition.x + distance * math.cos(self.direction), self.currentposition.y + distance * math.sin(self.direction)
+
+    local c = getcolor(self.color, self.rainbowmode)
+    local line = newline(self.currentposition.x, self.currentposition.y, x, y, c) 
     if self.drawing ~= false then
-        table.insert(self.lineSegments, line) 
+        table.insert(self.linesegments, line) 
     end
 
-    self.currentPosition.x, self.currentPosition.y = x, y
+    self.currentposition.x, self.currentposition.y = x, y
+    return self
 end
 
 function turtle:backward(distance)
     self:forward(-distance)
+    return self
 end
 
 function turtle:go_to(x, y)
-    local line = newline(self.currentPosition.x, self.currentPosition.y, x, y)
-    table.insert(self.lineSegments, line)
+    local c = self.getcolor(self.color, self.rainbowmode)
+    local line = newline(self.currentposition.x, self.currentposition.y, x, y, c)
+    table.insert(self.linesegments, line)
+    return self
 end
 
 function turtle:right(angle)
     self.direction = self.direction - math.rad(angle)
+    return self
 end
 
 function turtle:left(angle)
     self.direction = self.direction + math.rad(angle)
+    return self
 end
 
-return turtle
+return setmetatable({new = new},
+{__call = function(_, ...) return new(...) end})
+
