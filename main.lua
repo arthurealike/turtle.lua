21bc883fb6de655cd260fcba1b88a4a139d55cda
arthurah <thebestarthure@hotmail.com>
2020-07-28 12:00:21 +0300

    Fix turtle

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local Turtle = require "turtle"
 
-local koch = Turtle(love.graphics.getWidth() / 2 - 150, love.graphics.getHeight() / 2 - 150, 100)
+local koch = Turtle(love.graphics.getWidth() / 2 - 150, love.graphics.getHeight() / 2 - 150)
+local heart = Turtle()
+local line = Turtle()
 
 local function snowflake(t, l, c)
     if c > 7 then
@@ -12,19 +14,22 @@ local function snowflake(t, l, c)
     end
     l = l / 3
     snowflake(t, l, c-1)
-    t:rot(-60)
+    t:left(60)
     snowflake(t, l, c-1)
-    t:rot(120)
+    t:right(120)
     snowflake(t, l, c-1)
-    t:rot(-60)
+    t:left(60)
     snowflake(t, l, c-1)
 end
 
 function love.load()
+    koch:speed(1):color(1,0,1)
     for i = 1, 3 do
-        snowflake(koch, 300, 4)
+        snowflake(koch, 300, 1)
         koch:rot(120)
     end
+
+    line:speed(20):fd(250)
 end
 
 function love.update(dt)
@@ -32,4 +37,5 @@ end
 
 function love.draw()
     koch:draw()
+    line:draw()
 end
--- turtle.lua
+++ turtle.lua
@@ -1,6 +1,8 @@
 local Vector2 = require "vector"
 local Path = require "path"
 
+_TURTLEIMAGE = "turtle.png"
+
 local turtle = {}
 turtle.__index = turtle
 
@@ -13,22 +15,53 @@ local loadSprite = function (name)
     end
 end
 
-local function new(x, y, speed)
-    pos = Vector2(x, y) or Vector2(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
+local function new(x, y, speed, color)
+    _x, _y = x or love.graphics.getWidth() / 2, y or love.graphics.getHeight() / 2
+    pos = Vector2(_x, _y) 
+    color = color or {1, 1, 1}
     return setmetatable(
     {
         _pos = pos,
-        _sprite = loadSprite("turtle.png") ,
-        _speed = speed or 20 ,
+        _sprite = loadSprite(_TURTLEIMAGE) ,
+        _speed = speed or 10 ,
         _path = Path(pos) ,
+        _color = color ,
         _ratio = 0 ,
         _angle = 0 
 
     }, turtle)
 end
 
+function turtle:speed(speed)
+    self._speed = speed
+    return self
+end
+
+function turtle:color(...)
+    self._rainbowmode = false
+    local c = self._color
+    local nargs = select("#", ...)
+    if nargs == 3 then
+        c = {...}
+    elseif nargs == 1 then 
+        c = ... 
+    end
+    self._color = c
+    return self
+end
+
 function turtle:fd(d)
     return self:forward(d)
+end
+
+function turtle:right(deg)
+    self._angle = self._angle + math.rad(deg)
+    return self
+end
+
+function turtle:left(deg)
+    self._angle = self._angle - math.rad(deg)
+    return self
 end
 
 function turtle:forward(d)
@@ -46,12 +79,18 @@ end
 
 function turtle:draw()
     dt = love.timer.getDelta()
-    local ratio = dt * self._speed / 1000
+    local ratio =  self._speed / 100
 
     self._path:addRatio(ratio)
-    self._pos = self._path.lastPos:clone()
+    if self._path.lastPos then
+        self._pos = self._path.lastPos:clone()
+    end
+    love.graphics.setColor(self._color)
     self._path:draw()
-    love.graphics.draw(self._sprite, self._pos.x, self._pos.y, self._path:angle(), 1, 1, 8, 8)
+    love.graphics.setColor(1, 1, 1)
+    if self._sprite then
+        love.graphics.draw(self._sprite, self._pos.x, self._pos.y, self._path:angle(), 1, 1, 8, 8)
+    end
 end
 
 function turtle:print()
