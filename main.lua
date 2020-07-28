cb6638383cd153dbe2c8f0af62bb26244cd52441
Ahmet B. Badın <ahmet.badin@gmail.com>
2020-07-28 16:13:40 +0300

    path is removed - nodes are now handled by the turtle

--- main.lua
+++ main.lua
@@ -1,41 +1,88 @@
 local Turtle = require "turtle"
 
-local koch = Turtle(love.graphics.getWidth() / 2 - 150, love.graphics.getHeight() / 2 - 150)
-local heart = Turtle()
-local line = Turtle()
+local koch = Turtle("snowflake", love.graphics.getWidth() / 2 - 150, love.graphics.getHeight() / 2 - 150)
+local circle = Turtle("circle")
+local line = Turtle("line", 0)
 
 local function snowflake(t, l, c)
     if c > 7 then
-        print("Do not exceed number 7")
+        print("error: do not exceed number 7")
         return nil
     elseif c < 1 then
-        t:fd(l)
+        t:f(l)
         return nil
     end
     l = l / 3
     snowflake(t, l, c-1)
-    t:left(60)
+    t:rl(60)
     snowflake(t, l, c-1)
-    t:right(120)
+    t:rr(120)
     snowflake(t, l, c-1)
-    t:left(60)
+    t:rl(60)
     snowflake(t, l, c-1)
+end
+
+function initSnowflake(speed)
+    koch:setCallback(function ()
+        print ("snowflake is fully drawn")
+    end)
+
+    koch:speed(speed or 240):color(1,0,1)
+    for i = 1, 3 do
+        snowflake(koch, 300, 4)
+        koch:rot(120):color(i/3,0,i/3)
+    end
+end
+
+function initLine(speed)
+    line:setCallback(function ()
+        print ("line is fully drawn")
+    end)
+
+    line:speed(speed or 100)
+    for i = 1, love.graphics.getWidth(), 15 do
+        line:speed(math.max(10, i / 8)):f(15):color(math.random(), math.random(), math.random())
+    end
+end
+
+function initCircle(radius, speed)
+    circle:setCallback(function ()
+        print ("circle is fully drawn")
+    end)
+
+    local s = 2 * math.pi * radius / 360 
+    circle:speed(speed or 20)
+    for i = 1, 360, 1 do
+        circle:speed(math.max(10, i / 6)):f(s):rr(1):color(math.random(), math.random(), math.random())
+    end
 end
 
 function love.load()
-    koch:speed(1):color(1,0,1)
-    for i = 1, 3 do
-        snowflake(koch, 300, 1)
-        koch:rot(120)
-    end
-
-    line:speed(20):fd(250)
+    initSnowflake()
+    initLine()
+    initCircle(100)
 end
 
 function love.update(dt)
+    koch:update(dt)
+    line:update(dt)
+    circle:update(dt)
 end
 
 function love.draw()
     koch:draw()
     line:draw()
+    circle:draw()
+end
+
+function love.keypressed(key)
+    if key == "space" then
+        koch:toggle()
+        line:toggle()
+        circle:toggle()
+    elseif key == "c" then
+        koch:clear()
+        line:clear()
+        circle:clear()
+    end
 end
--- turtle.lua
+++ turtle.lua
@@ -1,5 +1,4 @@
 local Vector2 = require "vector"
-local Path = require "path"
 
 _TURTLEIMAGE = "turtle.png"
 
@@ -15,30 +14,121 @@ local loadSprite = function (name)
     end
 end
 
-local function new(x, y, speed, color)
+local function Node(x, y)
+    return {
+        _pos = Vector2(x, y) ,
+        _distance = 0 ,
+        _angle = 0 ,
+        _color = nil ,
+        _speed = 0 ,
+        _distance = 0
+    }
+end
+
+local function new(name, x, y, callback)
     _x, _y = x or love.graphics.getWidth() / 2, y or love.graphics.getHeight() / 2
     pos = Vector2(_x, _y) 
-    color = color or {1, 1, 1}
     return setmetatable(
     {
-        _pos = pos,
+        _name = name ,
+        _pos = pos ,
+        _currentPos = pos ,
         _sprite = loadSprite(_TURTLEIMAGE) ,
-        _speed = speed or 10 ,
-        _path = Path(pos) ,
-        _color = color ,
+        _speed = 10 ,
+        _nodes = {} ,
+        _color = {1, 1, 1} ,
         _ratio = 0 ,
-        _angle = 0 
+        _angle = 0 ,
+        _drawAngle = 0 ,
+        _currentDistance = 0 ,
+        _totalDistance = 0 ,
+        _dt = 0 ,
+        _playing = false ,
+        _nodeIndex = -1 ,
+        _lastNodeDrawPos = nil ,
+        _finalized = false ,
+        _callback = callback
 
     }, turtle)
 end
 
-function turtle:speed(speed)
-    self._speed = speed
+function turtle:_createNode(x, y)
+    local node = Node(x, y)
+    node._speed = self._speed
+    node._color = self._color
+    node._angle = self._angle
+    return node
+end
+
+function turtle:setCallback(callback) self._callback = callback end
+
+function turtle:_forward(d)
+    local pos = self._pos
+    if next(self._nodes) ~= nil then
+        pos = self._nodes[#self._nodes]._pos
+    end
+    
+    pos = addScalarWithAngle(pos, d, self._angle)
+    self._nodes[#self._nodes+1] = self:_createNode(pos.x, pos.y)
+
+    self:_calculateTotalDistance()
     return self
 end
 
-function turtle:color(...)
-    self._rainbowmode = false
+function turtle:undo(c)
+    c = c or 1
+
+    for i = #self._nodes, math.max(0, #self._nodes - c), -1 do
+        table.remove(self._nodes, i)
+    end
+
+    self._nodeIndex = math.min(#self._nodes, self._nodeIndex)
+    self:_calculateTotalDistance()
+    return self
+end
+
+function turtle:_calculateTotalDistance()
+    local dist = 0
+    local lastPos = self._pos
+    for i = 1, #self._nodes, 1 do
+        local node = self._nodes[i]
+        local vd = node._pos:distance(lastPos)
+
+        node._distance = vd
+        dist = dist + vd
+
+        lastPos = node._pos
+    end
+    self._totalDistance = dist
+end
+
+function turtle:clear()
+    self._currentPos = self._pos
+    self._currentDistance = 0
+    self._playing = false
+    self._nodeIndex = -1
+    self._lastNodeDrawPos = nil
+    self._dt = 0
+    self._finalized = false
+end
+
+function turtle:play() self._playing = true end     -- Play
+function turtle:pause() self._playing = false end   -- Pause
+function turtle:toggle() self._playing = not self._playing end
+function turtle:tl() return self:rot(-90) end       -- Turn left
+function turtle:tr() return self:rot(90) end        -- Turn right
+function turtle:f(d) return self:_forward(d) end    -- Forward
+function turtle:rl(a) return self:rot(-a) end       -- Rotate left
+function turtle:rr(a) return self:rot(a) end        -- Rotate right
+function turtle:rot(deg)                            -- Rotate by degree
+    self._angle = self._angle + math.rad(deg)
+    return self
+end
+function turtle:speed(speed)                        -- Set speed
+    self._speed = speed
+    return self
+end
+function turtle:color(...)                          -- Set color
     local c = self._color
     local nargs = select("#", ...)
     if nargs == 3 then
@@ -50,46 +140,66 @@ function turtle:color(...)
     return self
 end
 
-function turtle:fd(d)
-    return self:forward(d)
-end
-
-function turtle:right(deg)
-    self._angle = self._angle + math.rad(deg)
-    return self
-end
-
-function turtle:left(deg)
-    self._angle = self._angle - math.rad(deg)
-    return self
-end
-
-function turtle:forward(d)
-    self._path:addNodeWithDistance(d, self._angle)
-    return self
-end
-
-function turtle:l() return self:rot(-90) end
-function turtle:r() return self:rot(90) end
-
-function turtle:rot(deg)
-    self._angle = self._angle + math.rad(deg)
-    return self
+function turtle:_drawPath()
+    local lastPos = self._pos
+    for i = 1, self._nodeIndex, 1 do
+        local node = self._nodes[i]
+        love.graphics.setColor(node._color)
+        if i == self._nodeIndex then
+            love.graphics.line(lastPos.x, lastPos.y, self._lastNodeDrawPos.x, self._lastNodeDrawPos.y)
+            break
+        else
+            love.graphics.line(lastPos.x, lastPos.y, node._pos.x, node._pos.y)
+        end
+        lastPos = node._pos
+    end
 end
 
 function turtle:draw()
-    dt = love.timer.getDelta()
-    local ratio =  self._speed / 100
-
-    self._path:addRatio(ratio)
-    if self._path.lastPos then
-        self._pos = self._path.lastPos:clone()
-    end
-    love.graphics.setColor(self._color)
-    self._path:draw()
-    love.graphics.setColor(1, 1, 1)
+    self:_drawPath()
+    love.graphics.setColor({1,1,1})
     if self._sprite then
-        love.graphics.draw(self._sprite, self._pos.x, self._pos.y, self._path:angle(), 1, 1, 8, 8)
+        love.graphics.draw(self._sprite, self._currentPos.x, self._currentPos.y, self._drawAngle, 1, 1, 8, 8)
+    end
+end
+
+function turtle:update(dt)
+    if next(self._nodes) == nil then return end
+    if self._finalized or not self._playing then return end
+
+    local lastPos = self._pos
+    local node = self._nodes[math.max(self._nodeIndex, 1)]
+    local speed = node._speed
+    local angle = node._angle
+
+    self._dt = self._dt + dt * speed
+
+    local ratio = math.min(1.0, math.max(0.0, self._dt / self._totalDistance))
+    local reachDistance = self._totalDistance * ratio
+
+    for i = 1, #self._nodes, 1 do
+        local node = self._nodes[i]
+        local diff = reachDistance - node._distance
+
+        if diff < 0 then
+            self._nodeIndex = i
+            self._lastNodeDrawPos = lerp(lastPos, node._pos, reachDistance / node._distance)
+            self._currentPos = self._lastNodeDrawPos
+            break
+        end
+
+        reachDistance = diff
+        lastPos = node._pos
+        self._currentPos = lastPos
+    end
+    
+    self._drawAngle = angle
+
+    if ratio == 1.0 and not self._finalized then
+        self._nodeIndex = #self._nodes
+        self._lastNodeDrawPos = self._nodes[self._nodeIndex]._pos
+        self._finalized = true
+        if self._callback ~= nil then self._callback() end
     end
 end
 
